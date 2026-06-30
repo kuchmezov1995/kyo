a0325acfd2961237659eb8ff02ddc7a23e2814d8
kuchmezov1995 <kuchmezov1995@gmail.com>
2026-07-01 02:54:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -67,3 +67,11 @@ function animate() {
 }
 
 animate();
+// ===== CUSTOM CURSOR =====
+
+const cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = e.clientX + "px";
+  cursor.style.top = e.clientY + "px";
+});
