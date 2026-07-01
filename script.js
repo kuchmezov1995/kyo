2963b956ad47f2db3a4bac71ce88ec25be548c73
kuchmezov1995 <kuchmezov1995@gmail.com>
2026-07-01 05:07:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,19 @@ window.addEventListener("load", () => {
 document.querySelectorAll("nav a").forEach((button, index) => {
     button.style.animationDelay = `${index * 120 + 800}ms`;
 });
+
+const cursor = document.querySelector(".cursor");
+const dot = document.querySelector(".cursor-dot");
+
+document.addEventListener("mousemove", (e) => {
+
+    const x = e.clientX;
+    const y = e.clientY;
+
+    dot.style.left = x + "px";
+    dot.style.top = y + "px";
+
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+
+});
