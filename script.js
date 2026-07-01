f3856495f2776fb5350480ee74903ed01a4c0b0a
kuchmezov1995 <kuchmezov1995@gmail.com>
2026-07-01 05:38:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-// ===== Live Clock =====
-const clock = document.querySelector(".clock");
-
-function updateClock() {
-    if (!clock) return;
-
-    const now = new Date();
-
-    const h = String(now.getHours()).padStart(2, "0");
-    const m = String(now.getMinutes()).padStart(2, "0");
-
-    clock.textContent = `${h}:${m}`;
-}
-
-updateClock();
-setInterval(updateClock, 1000);
-
 
 // ===== Fade In =====
 window.addEventListener("load", () => {
