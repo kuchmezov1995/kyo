6a376b4c85b81b49f93e649e627cfe626386cd35
kuchmezov1995 <kuchmezov1995@gmail.com>
2026-07-01 04:51:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // ===== Live Clock =====
-
 const clock = document.querySelector(".clock");
 
 function updateClock() {
+    if (!clock) return;
 
     const now = new Date();
 
@@ -10,27 +10,19 @@ function updateClock() {
     const m = String(now.getMinutes()).padStart(2, "0");
 
     clock.textContent = `${h}:${m}`;
-
 }
 
 updateClock();
-
 setInterval(updateClock, 1000);
 
 
 // ===== Fade In =====
-
 window.addEventListener("load", () => {
-
     document.body.classList.add("loaded");
-
 });
 
 
 // ===== Links Animation =====
-
 document.querySelectorAll("nav a").forEach((button, index) => {
-
     button.style.animationDelay = `${index * 120 + 800}ms`;
-
 });
